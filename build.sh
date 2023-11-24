1f8686b3d5e6113ed535b818f5c1433ac9d407fa
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2023-11-24 12:11:27 +0100

    fix: send errors log as well

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ PLUGIN=$( yq e '.name' plugin.codefly.yaml)
 VERSION=$(yq e '.version' plugin.codefly.yaml
 )
 echo Building ${PLUGIN}:${VERSION}
-go build -o ~/.codefly/plugins/services/codefly.ai/${PLUGIN}:${VERSION} *.go
+go build -o ~/.codefly/plugins/services/codefly.ai/${PLUGIN}__${VERSION} *.go
